61d40e69586e2729dd35097a2b022982a3ef40d6
lyssdieye10 <salimatadieye7@gmail.com>
2026-04-21 02:50:47 +0200

    Add files via upload

---
+++ cv1.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+st.header(" Geomorphologue littorale et Technicienne superieur en Géomatique, je conjugue une solide compréhension des dynamiques côtières à une maîtrise avancée des outils de mesure topographique et de traitement de données spatiales. Spécialisée dans l'analyse des risques d'érosion et de submersion, je réalise des diagnostics précis (levés GNSS, modélisation SIG, télédétection) pour accompagner la gestion durable et le suivi technique des littoraux ")
+st.header("Formation")
+st.success("Licence en geographie à l'Université Cheikh Anta Diop de Dakar")
+st.success("Master en Geographie, option geomorphologie")
+st.success("Brevet Technicienne Superieur en Geomatique")
+with st.sidebar:
+    st.sidebar.image("lyss.jpg",width=170)
+    st.header("Information Personnelle")
+    st.write("Prénom: Salimata")
+    st.write("Nom: Dieye")
+    st.write("Addresse: Saint-Louis/Sénégal")
+    st.write("Email:salimatadieye7@gmail.com")
+    st.write("senegalaise")
+    st.header("Competenses")
+    st.write("Cartographie")
+    st.write("Numerisation")
+    st.write("Realisation d'une coupe géologique")
+    st.write("Collecte de Données")
+    st.write("Outils SIG (Qgis,Arcgis)")
+    st.write("Streamlit")
+    st.write("Dessin de Plan")
+    st.write("informatique")
+    st.header("Langue")
+    st.markdown("""
+    * Wolof: Avancé""")
+    st.write("""
+    * angais:intermediaire""")
+    st.write("""
+    * français:Avancé""")
+    st.header("Centre d'interet")
+    st.write("lecture")
+    st.write("Sport")
+    
+    
+    
